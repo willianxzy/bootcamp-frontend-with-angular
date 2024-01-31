bec51be48b2af6c54e9ff3b74da78be2a0370ab1
willianxzy <willianlittig14@gmail.com>
2024-01-31 05:18:42 -0300

    refactor: Alteração no comentário para uma resposta mais clara ao leitor.

--- introd_JS/condicionais/condicionais.js
+++ introd_JS/condicionais/condicionais.js
@@ -12,5 +12,5 @@ const camisetaLemaoAzul = false;
 const numero = 10; /*Aqui nessa variável vamos declarar o nosso número que queremos ver se é true ou false utilizando a condição de resto da divisão;*/
 
 const numeroPar = (numero % 2) === 0; 
-/* Portanto aqui criamos uma variável chamada (numeroPar) em que nela se pega o resultado da variável número e divide o resto da divisão(simbolo de porcentagem) por 2 e verificamos se ele é igual a 0, se for é true, se não for é false.*/
+/* Portanto aqui criamos uma variável chamada (numeroPar) em que nela se pega o resultado da variável (número) e divide o resto da divisão(simbolo de porcentagem) por 2 e verificamos se ele é igual a 0, se for é true, se não for é false.*/
 console.log(numeroPar);
