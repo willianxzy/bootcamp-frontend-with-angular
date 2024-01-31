aa425ba3bc9c22557ca1c6413a7ba5446799a53a
willianxzy <willianlittig14@gmail.com>
2024-01-31 05:04:51 -0300

    feat: Introdução a condicionais.

---
+++ introd_JS/condicionais/condicionais.js
@@ -0,0 +1,16 @@
+//Vamos ver um emplo na prática de boolean 
+const camisetaRenanAzul = true;
+const camisetaLemaoAzul = false;
+// este é um exemplo de boolean, pois ele é o conceito de true e false, ou seja, ele é nosso binário.
+
+// Vamos a um exemplo simples de condicional abaixo:
+10 > 5
+// Acima temos que 10 é maior que 5, essa condição é verdadeira ou falsa? com certeza true né.
+
+// Vamos ver um exemplo utilizando o resto da divisão.
+
+const numero = 10; /*Aqui nessa variável vamos declarar o nosso número que queremos ver se é true ou false utilizando a condição de resto da divisão;*/
+
+const numeroPar = (numero % 2) === 0; 
+/* Portanto aqui criamos uma variável chamada (numeroPar) em que nela se pega o resultado da variável número e divide o resto da divisão(simbolo de porcentagem) por 2 e verificamos se ele é igual a 0, se for é true, se não for é false.*/
+console.log(numeroPar);
