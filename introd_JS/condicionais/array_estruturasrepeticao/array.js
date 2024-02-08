1cfe2676aaff6e0fd3421986c8fa0db158ea6c08
willianxzy <willianlittig14@gmail.com>
2024-02-08 06:13:10 -0300

    feat: Introdução a Arrays e Estruturas de Repetição + Execução de for em uma Array de notas.

---
+++ introd_JS/condicionais/array_estruturasrepeticao/array.js
@@ -0,0 +1,34 @@
+const alunos = ['pedro','jose','julia']
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------
+//ADICIONANDO E REMOVENDO ITENS DO NOSSO ARRAY
+
+alunos.push('willian'); //desta maneira conseguimos adicionar mais um item a nossa lista utilizando o nomedavariavel.push;
+alunos[4]= 'Matheus'; // em contra partida conseguimos adicionar um item a nossa lista, e já inserirmos na posição que desejamos.
+alunos.push('teste');
+alunos.push(10);
+//conseguimos ter diferentes tipos de itens em nossa lista, desde strings até números.
+console.log(alunos.pop()); //Desta maneira estamos pedindo para ele remover o último item de nossa lista, porém imprimi-lo para podermos utilizá-lo.
+alunos.shift(); //Fazemos a mesma função anterior, porém com o primeiro item da nossa lista, mas dessa vez sem imprimir o mesmo.
+
+
+
+console.log(alunos);
+console.log(alunos[3]); //solicitamos que imprima o item 0 da nossa lista na variável alunos.
+//item não existente na nossa lista, terminal imprime como undefined.
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+//ESTRUTURAS DE REPETIÇÃO
+
+const notas = [];
+
+notas.push(5);
+notas.push(4);
+notas.push(7);
+notas.push(8);
+notas.push(10);
+console.log();
+
+for (let i = 0; i < notas.length; i++) {
+    console.log(notas[i]);
+}
