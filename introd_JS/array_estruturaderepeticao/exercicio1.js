d39fb71a8f29ec39a854d492de443e68dcb9a9b2
willianxzy <willianlittig14@gmail.com>
2024-02-12 11:10:56 -0300

    feat: Aplicando conhecimentos utilizando estrutura de repetição for loop para desevolver a tabuada de um determinado número inserido pelo usuário.

---
+++ introd_JS/array_estruturaderepeticao/exercicio1.js
@@ -0,0 +1,8 @@
+// 1 - Crie um programa que dado um número imprima a sua tabuada. 
+
+const inserirNumero = 2;
+
+for (let i = 1; i <= 10; i++) {
+    const resultado = i*inserirNumero; 
+    console.log(`${i} x ${inserirNumero} = ${resultado}`)
+};
