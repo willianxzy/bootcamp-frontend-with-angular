082c1038cfef5fb8666bcdc591bec9305ba23269
willianxzy <willianlittig14@gmail.com>
2024-02-13 10:08:26 -0300

    feat: Conteúdo introduzindo as funções auxiliares e como usa-lás.

---
+++ introd_JS/importação/funcoes-auxiliares.js
@@ -0,0 +1,12 @@
+
+//vamos mocar um valor aqui, essa expressão mocar se dá para simular algo.
+function gets(){
+    return 10; 
+}
+
+function print(texto){
+    console.log(texto);
+}
+
+module.exports = { gets, print };
+//vamos exportar essas duas funções para nosso arquivo main para podermos utiliza-lás por lá.
