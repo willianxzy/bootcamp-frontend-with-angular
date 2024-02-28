fa5782b7f987e3b50e1527aa7f7d5bbe2fec3f7c
willianxzy <willianlittig14@gmail.com>
2024-02-28 06:19:36 -0300

    refactor: solicitando novas requisições para adquirir detalhes dos pokemons.

--- proj1_pokedex/assets/js/poke-api.js
+++ proj1_pokedex/assets/js/poke-api.js
@@ -6,5 +6,14 @@ pokeApi.getPokemons = (offset = 0 , limit = 10 ) => {           //Vamos colocar 
     .then((response) => response.json())
     .then((jsonBody) => jsonBody.results)
     .catch((error) => console.error(error))
-    .finally(console.log('Requisição Finalizada!'))
+    .finally(console.log('Name Request Completed!'))
 }
+
+Promise.all ([
+    fetch ('https://pokeapi.co/api/v2/pokemon/1'),
+    fetch ('https://pokeapi.co/api/v2/pokemon/2'),
+    fetch ('https://pokeapi.co/api/v2/pokemon/3'),
+    fetch ('https://pokeapi.co/api/v2/pokemon/4')
+]).then((results) => {
+    console.log(results)
+})
