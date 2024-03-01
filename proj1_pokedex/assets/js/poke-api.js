928adeac1117608d301da0a227591bbddf97ca66
willianxzy <willianlittig14@gmail.com>
2024-03-01 05:15:00 -0300

    feat: manipulando requisições (order, name, img)

--- proj1_pokedex/assets/js/poke-api.js
+++ proj1_pokedex/assets/js/poke-api.js
@@ -1,19 +1,19 @@
 const pokeApi = {}
 
-pokeApi.getPokemons = (offset = 0 , limit = 10 ) => {           //Vamos colocar padrões para o offset e para o limit que assim que forem lidos, eles vão utilizar os valores padrões
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+}
+
+pokeApi.getPokemons = (offset = 0 , limit = 10 ) => {           //padrões para o offset e para o limit que assim que forem lidos, eles vão utilizar os valores padrões
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
     return fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody.results)
+        .then ((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => pokemonsDetails)
+
     .catch((error) => console.error(error))
     .finally(console.log('Name Request Completed!'))
 }
-
-Promise.all ([
-    fetch ('https://pokeapi.co/api/v2/pokemon/1'),
-    fetch ('https://pokeapi.co/api/v2/pokemon/2'),
-    fetch ('https://pokeapi.co/api/v2/pokemon/3'),
-    fetch ('https://pokeapi.co/api/v2/pokemon/4')
-]).then((results) => {
-    console.log(results)
-})
