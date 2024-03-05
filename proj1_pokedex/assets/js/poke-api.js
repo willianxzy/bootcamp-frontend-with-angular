5ed8ff2256e2344cd09e1c00f2780158fcee1f29
willianxzy <willianlittig14@gmail.com>
2024-03-04 21:11:53 -0300

    feat: Criação do nosso pokemonModel + function de conversão pokeApi para um novo pokemon.

--- proj1_pokedex/assets/js/poke-api.js
+++ proj1_pokedex/assets/js/poke-api.js
@@ -1,8 +1,24 @@
 const pokeApi = {}
 
+function convertPokeApiDetailToPokemon(pokeDetail) {
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDetail.order
+    pokemon.name = pokeDetail.name
+    pokemon.types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types 
+    pokemon.type = type 
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+    return pokemon
+}
+
 pokeApi.getPokemonDetail = (pokemon) => {
     return fetch(pokemon.url)
         .then((response) => response.json())
+        .then(convertPokeApiDetailToPokemon)
 }
 
 pokeApi.getPokemons = (offset = 0 , limit = 10 ) => {           //padrões para o offset e para o limit que assim que forem lidos, eles vão utilizar os valores padrões
