14b3261a4978c178997bf9abedc97cf0716f1f16
willianxzy <willianlittig14@gmail.com>
2024-03-07 05:15:20 -0300

    feat: add pokemon type to li + adicionando classes de cores para dinamizar com o background + add capitalize name

--- proj1_pokedex/assets/js/main.js
+++ proj1_pokedex/assets/js/main.js
@@ -5,13 +5,13 @@ const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
 function convertPokemonsToLi(pokemon) {
     return `
-            <li class="pokemon">
+            <li class="pokemon ${pokemon.type}">
                 <span class="number">#${pokemon.number}</span>
                 <span class="name">${pokemon.name}</span>
 
                 <div class="detail">
                     <ol class="types">
-                        ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
                     </ol>
                     <img src="${pokemon.photo}" 
                     alt="${pokemon.name}">
