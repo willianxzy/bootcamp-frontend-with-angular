eda07071bdf7b17593f31ca38375a2fdfdbfac16
willianxzy <willianlittig14@gmail.com>
2024-03-17 21:38:45 -0300

    feat: alterações no pokemon-profile + add button toGoBack para página anterior

--- proj1_pokedex/assets/js/pokeDetails/main.js
+++ proj1_pokedex/assets/js/pokeDetails/main.js
@@ -19,19 +19,25 @@ const loadPokemonProfile = () => {
               > <i class="fa-solid fa-chevron-left" style="color: #000"></i
               ></a>
             <img class="logo" src="https://user-images.githubusercontent.com/29473781/180619084-a56960ab-7efa-4e34-9d33-4e3e581d62ff.png" alt="pokemon logo"/>
-          </header>
-             <div> 
-             <h1 style="color:#000;">${pokemon.name}<h1>
-             ${pokemon.number}
-             </br></br>
-             ${about}
-             </br></br>
+            <div class="buttonBack">
+            <button onclick="history.back()" id="toGoBack" type="button">
+                <----
+            </button>
+        </div>
+            </header>
+             <div class= "pokeNumber"> #${pokemon.number} </div>
+             <h1 class="pokeName">${pokemon.name}</h1>
              ${pokemon.types}
-             ${weight} kg
-             ${height} cm
-             ${pokemon.shape}
-             ${pokemon.habitat}
-             <img src="${pokemon.image}" alt="${pokemon.name}'s appearance" />
+             
+             <br><br><br>
+             <div class="pokedescription"> ${about} <div>
+             <br><br>
+             ${pokemon.types}
+             <p>${weight} kg </p>
+             <p>${height} cm </p>
+             <p>${pokemon.shape}</p>
+             <p>${pokemon.habitat}</p>
+             <img class="pokePhoto" src="${pokemon.image}" alt="${pokemon.name}'s appearance" />
              </div>
             </div>
         `;
@@ -40,3 +46,8 @@ const loadPokemonProfile = () => {
 };
 
 loadPokemonProfile();
+
+document.getElementById("buttonBack").addEventListener("click", 
+function (){
+  history.back();
+});
