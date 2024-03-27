5895a302c7bc59f8078ba4df8699a07c1fafcb5f
willianxzy <willianlittig14@gmail.com>
2024-03-27 13:33:12 -0300

    feat: add getAttribute

--- fundamentos_angular/web_components/desafio1/src/components/TituloDinamico.js
+++ fundamentos_angular/web_components/desafio1/src/components/TituloDinamico.js
@@ -1,22 +1,22 @@
 class TituloDinamico extends HTMLElement {
-    constructor(){
+    constructor () {
         super();
 
         const shadow = this.attachShadow({mode: "open"});
 
         //base component
-        const componentRoot = document.createElement('h1');
-        componentRoot.textContent = 'Willian';
+        const componentRoot = document.createElement("h1");
+        componentRoot.textContent = this.getAttribute("titulo");
 
         //estilização component
-        const style = document.createElement('style');
+        const style = document.createElement("style");
         style.textContent = `
         h1 {
             color: red; 
         }
         `;
 
-        //enviar p/ shadow
+        //enviar para shadow
         shadow.appendChild(componentRoot);
         shadow.appendChild(style);
     };
