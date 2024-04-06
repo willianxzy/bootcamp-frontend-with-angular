5061dbad5297cfbf2ea2a6d29828055339bc6dd8
willianxzy <willianlittig14@gmail.com>
2024-04-06 06:17:03 -0300

    feat: add object tipagem produtoLoja

--- fundamentos_angular/introd_typescript/objetos/index.ts
+++ fundamentos_angular/introd_typescript/objetos/index.ts
@@ -7,3 +7,21 @@ let produto: object = {
 };
 
 // quando declaramos uma variável de object podemos receber qualquer tipo de objeto, porém quando fazemos isso estamos passando um objeto sem previsibilidade.
+
+
+// criando um object tipado com previsibilidade
+
+// primeiro devemos criar um type para declarar quais os conteúdos do nosso object tipado
+type produtoLoja = {
+	nome: string;
+	valor: number;
+	unidades: number;
+};
+
+let meuProduto: produtoLoja = {
+    nome: 'Tênis',
+	valor: 149.99,
+	unidades: 10,
+};
+
+// Ou seja, criamos um object no qual seus itens foram tipados pelo type produtoLoja, e o nosso objeto meuProduto segue os parâmetros desse type.
