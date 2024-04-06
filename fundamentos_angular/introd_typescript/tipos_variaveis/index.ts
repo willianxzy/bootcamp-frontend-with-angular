0fb4353c27a93fc6b151d4671ac202defeed07f3
willianxzy <willianlittig14@gmail.com>
2024-04-06 05:48:22 -0300

    feat: variaveis e seus tipos

---
+++ fundamentos_angular/introd_typescript/tipos_variaveis/index.ts
@@ -0,0 +1,17 @@
+// Quando falamos de variáveis temos 3 tipos delas
+
+// > Boolean 
+let ligado: boolean = false;
+let desligado = true;
+
+// > Number
+let idade: number = 18;
+let idade2 = 18;
+
+// > String 
+let nome: string = 'willian';
+let nome2 = 'willian';
+
+// Ao declarar essa variável e tipar ela, estamos limitando ela a apenas aquele tipo de tipagem que atribuímos a ela. 
+// ex: se tipamos a variável nome com string, não se pode atribuir números a ela, apenas strings.
+
