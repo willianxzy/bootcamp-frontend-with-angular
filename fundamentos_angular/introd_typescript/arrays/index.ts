0c236d807e4bbcbc24b4fc626c08a2aa002192d4
willianxzy <willianlittig14@gmail.com>
2024-04-06 06:37:25 -0300

    feat: métodos para trabalhar com arrays

--- fundamentos_angular/introd_typescript/arrays/index.ts
+++ fundamentos_angular/introd_typescript/arrays/index.ts
@@ -18,3 +18,11 @@ let dados3: (string | number)[] = ['Willian', 19, 'Julio', 18, 'Pedro', 18];
 let boleto: [string, number, number] = ['conta-agua', 179.67, 85374953]
 
 // portanto se deve seguir a seguência das declarações de acordo com nossa variável. Caso seja declarado de maneira diferente da qual declaramos em nossa variável irá dar erro.
+
+// Reaproveitando conhcimento do js
+// Métodos para trabalhar com Arrays
+
+// array.map  - Muito utilizado para mapear um array.
+// array.reduce - Executa a função de CallBack uma vez para cada elemento presente no Array, excluindo valores indefinidos.
+// array.pop - Remove o último elemento do array.
+// array.pop - Filtrar alguma coisa específica em seu array.
