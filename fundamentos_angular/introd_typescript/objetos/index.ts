68dbb501b5c4aab237719b3142d6598941d342af
willianxzy <willianlittig14@gmail.com>
2024-04-06 06:13:13 -0300

    feat: object sem previsibilidade.

---
+++ fundamentos_angular/introd_typescript/objetos/index.ts
@@ -0,0 +1,9 @@
+// object 
+
+let produto: object = {
+	name: 'Willian',
+	idade: 19,
+	cidade: 'Domingos Martins',
+};
+
+// quando declaramos uma variável de object podemos receber qualquer tipo de objeto, porém quando fazemos isso estamos passando um objeto sem previsibilidade.
