e15689dbaecf0a673c598d3d6454b98fdeedb123
willianxzy <willianlittig14@gmail.com>
2024-04-06 06:26:00 -0300

    feat: arrays e seus tipos

---
+++ fundamentos_angular/introd_typescript/arrays/index.ts
@@ -0,0 +1,6 @@
+// Temos 2 maneiras pela qual podemos declarar os arrays
+// 1ª Maneira
+let dados: string[] = ['Willian','Julio','Pedro'];
+
+// 2ª Maneira
+let dados2: Array<string> = ['Willian','Julio','Pedro'];
