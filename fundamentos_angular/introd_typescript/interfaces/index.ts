d0d457f43ffa8a3f010d357177535fefce59fed2
willianxzy <willianlittig14@gmail.com>
2024-04-06 07:23:14 -0300

    feat: readonly example

--- fundamentos_angular/introd_typescript/interfaces/index.ts
+++ fundamentos_angular/introd_typescript/interfaces/index.ts
@@ -19,3 +19,30 @@ const bot: robot = {
 	name: 'MegaBot',
 };
 
+// Podemos utilizar o “readonly” para definir que aquele parâmetro seja somente de leitura, ou seja, o mundo externo não pode modificar aquele parâmetro estabelecido.
+
+type robotEx2 = {
+    readonly id: number;
+    name: string;
+};
+
+interface robot2Ex2 {
+    readonly id: number;
+    name: string;
+};
+
+const bot1: robot = {
+    id: 1,
+    name: 'MegaBot1',
+};
+
+const bot2: robot2 = {
+    id: 2,
+    name: 'MegaBot2',
+};
+
+(bot1.name = 'MegaBot007')
+console.log(bot1);
+console.log(bot2);
+
+// portanto se tentamos manipular o name irá funcionar pois não utilizamos o readonly nele, porém, se for utilizar no id irá apresentar erro pois não podemos alterar em modo de somente leitura (readonly)
