c3cdf0ce1bbdb6aa3c668c3d7a3c12c63a9fd350
willianxzy <willianlittig14@gmail.com>
2024-04-06 06:39:01 -0300

    feat: trabalhando com dates

---
+++ fundamentos_angular/introd_typescript/date/index.ts
@@ -0,0 +1,10 @@
+let aniversario: Date = new Date('2004-08-14 05:00');
+	console.log(aniversario.toString());
+	
+	
+	// comando utilizado no terminal 
+	// npx src index.ts
+	
+	// comando utilizado para executar arquivo gerado pelo npx
+	// node nomeDoArquivo.js
+	// --> node index.js
