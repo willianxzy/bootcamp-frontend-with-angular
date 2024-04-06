6c44eceb92795fc180f384e2caf87ae9586996db
willianxzy <willianlittig14@gmail.com>
2024-04-06 07:34:25 -0300

    feat: utilizando interfaces

--- fundamentos_angular/introd_typescript/interfaces/index.ts
+++ fundamentos_angular/introd_typescript/interfaces/index.ts
@@ -36,7 +36,7 @@ const bot1: robot = {
     name: 'MegaBot1',
 };
 
-const bot2: robot2 = {
+const bot2: robot2Ex2 = {
     id: 2,
     name: 'MegaBot2',
 };
@@ -46,3 +46,31 @@ console.log(bot1);
 console.log(bot2);
 
 // portanto se tentamos manipular o name irá funcionar pois não utilizamos o readonly nele, porém, se for utilizar no id irá apresentar erro pois não podemos alterar em modo de somente leitura (readonly)
+
+// Quando utilizar Interfaces? 
+//Utilizamos mais os Interfaces quando estamos utilizando classes.
+//E devemos utilizar type quando queremos apenas typar nossas variáveis. 
+//Portanto quando vamos utilizar uma classe devemos utilizar interfaces para aparentar um “contrato” e assim nossa classe vai seguir aquele parâmetros estipulados em nossa interface.
+
+interface robot2 {
+	id: number;
+	name: string;
+	sayHello(): string;
+}
+
+class MegaBot implements robot2 {
+	id: number;
+	name: string;
+	sayHello(): string;
+	
+constructor(id: number, name: string) {
+		this.id = id;
+		this.name = name;
+		this.sayHello() {
+			return `Hello ${this.name}`
+		};
+    };	
+};
+
+const teste = new MegaBot(1, 'Willian');
+console.log(teste.sayHello());
