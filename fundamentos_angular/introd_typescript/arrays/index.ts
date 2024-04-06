4c233c3ffc0ce4084e7222a4a9104f268a715f3c
willianxzy <willianlittig14@gmail.com>
2024-04-06 06:28:44 -0300

    feat: arrays em multi types.

--- fundamentos_angular/introd_typescript/arrays/index.ts
+++ fundamentos_angular/introd_typescript/arrays/index.ts
@@ -4,3 +4,10 @@ let dados: string[] = ['Willian','Julio','Pedro'];
 
 // 2ª Maneira
 let dados2: Array<string> = ['Willian','Julio','Pedro'];
+
+// Trabalhando com arrays de multi types
+// conseguimos criar arrays que eles possuem multi typos  dentro do array
+
+let dados3: (string | number)[] = ['Willian', 19, 'Julio', 18, 'Pedro', 18];
+
+// Em TypeScript e JavaScript o | é lido como “ou”, sendo assim declaramos (string ou number). Portanto dessa maneira será aceito tanto string como number.
