abb92ba3fc912bf186c1b8bdfc7e2e8936faccdb
willianxzy <willianlittig14@gmail.com>
2024-04-06 07:14:47 -0300

    feat: funções em multi types

--- fundamentos_angular/introd_typescript/funcoes/index.ts
+++ fundamentos_angular/introd_typescript/funcoes/index.ts
@@ -7,3 +7,12 @@ function addNumber(x: number, y: number): number {
 let soma: number = addNumber(4,7);
 
 // Declaramos a tipagem para nosso x ser number, nosso y ser number e o resultado dessa função também ser do tipo number.
+
+// Funções em multi types
+// Podemos ter funções com Multi Types, e que ela retorne a variável que lhe foi proposta.
+
+function CallToPhone(phone: number | string): number | string {
+	return phone;
+};
+
+console.log(CallToPhone(27997356785));
