4123fad1572c9629e1545e601308b9bb98a3ac28
willianxzy <willianlittig14@gmail.com>
2024-04-06 05:57:08 -0300

    feat: variáveis abrangentes

--- fundamentos_angular/introd_typescript/tipos_variaveis/index.ts
+++ fundamentos_angular/introd_typescript/tipos_variaveis/index.ts
@@ -15,3 +15,16 @@ let nome2 = 'willian';
 // Ao declarar essa variável e tipar ela, estamos limitando ela a apenas aquele tipo de tipagem que atribuímos a ela. 
 // ex: se tipamos a variável nome com string, não se pode atribuir números a ela, apenas strings.
 
+// Temos também as variáveis abrangentes 
+
+// as variáveis abrangentes são 2 tipos 
+
+// > Any 
+let listaAleatoria: any = ['willian', 18, 29, 'Joaozinho', true]
+
+// na variável any podemos declarar qualquer coisa nela que isso será aceito sem nenhum erro.
+
+// > Void 
+let semNada: void 
+
+// na variavel void é um tipo de variável vazia, que não tem necessidade de retornar nada, somente executar a função que lhe foi declarada.
