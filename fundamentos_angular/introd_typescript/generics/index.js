8172b4153ae6bf109f5b796d087bc8c59d52f63a
willianxzy <willianlittig14@gmail.com>
2024-04-08 12:10:06 -0300

    feat: dinamizando array number e string.

--- fundamentos_angular/introd_typescript/generics/index.js
+++ fundamentos_angular/introd_typescript/generics/index.js
@@ -1,4 +1,5 @@
 // função p/ concatenar os arrays 
+// devemos adicionar qual o tipo que queremos que nosso array trabalhe, apenas number ou apenas string, portanto devemos tipar isso.
 function concatArray() {
     var _a;
     var itens = [];
@@ -13,5 +14,20 @@ var stringArray = concatArray('Willian', 'Ana', 'Fonseca');
 // conseguimos adicionar uma string dentro do nosso array de numeros, porém fazendo isso quebramos uma regra de fidelidade em nosso código.
 numberArray.push('Willian');
 console.log(numberArray);
+// e da mesma maneira conseguimos adicionar um número em nossa array de string, porém não é isso que queremos.
 stringArray.push(10);
 console.log(stringArray);
+//portanto vamos agora refazer nosso código para ver como que fazemos essa tipagem com nosso arrays e cada um com suas caracteristicas(number e string).
+function concatArrayDinamico() {
+    var _a;
+    var itens = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        itens[_i] = arguments[_i];
+    }
+    return (_a = new Array()).concat.apply(_a, itens);
+}
+;
+var numberArrayDinamico = concatArrayDinamico([1, 5], [3]);
+var stringArrayDinamico = concatArrayDinamico(['Willian', 'Ana'], ['Fonseca']);
+console.log(numberArrayDinamico);
+console.log(stringArrayDinamico);
--- fundamentos_angular/introd_typescript/generics/index.ts
+++ fundamentos_angular/introd_typescript/generics/index.ts
@@ -6,7 +6,7 @@ function concatArray(...itens: any[]): any[] {
 };
 
 const numberArray = concatArray([1,5], [3]);
-const stringArray = concatArray('Willian', 'Ana', 'Fonseca');
+const stringArray = concatArray(['Willian', 'Ana'], ['Fonseca']);
 
 // conseguimos adicionar uma string dentro do nosso array de numeros, porém fazendo isso quebramos uma regra de fidelidade em nosso código.
 numberArray.push('Willian')
@@ -16,3 +16,15 @@ stringArray.push(10)
 console.log(stringArray);
 
 //portanto vamos agora refazer nosso código para ver como que fazemos essa tipagem com nosso arrays e cada um com suas caracteristicas(number e string).
+
+// Primeira mudança no nosso código vamos adicionar as setas angulares com o "T" dentro delas e no lugar de Any vamos colocar o "T"
+function concatArrayDinamico<T>(...itens: T[]): T[] {
+    return new Array().concat(...itens);
+};
+
+// portanto agora dessa maneira conseguimos efetuar a tipagem de cada array, não sendo mais possível adicionar string em array de number e vice-versa.
+const numberArrayDinamico = concatArrayDinamico<number[]>([1,5], [3]);
+const stringArrayDinamico = concatArrayDinamico<string[]>(['Willian', 'Ana'], ['Fonseca']);
+
+console.log(numberArrayDinamico);
+console.log(stringArrayDinamico);
