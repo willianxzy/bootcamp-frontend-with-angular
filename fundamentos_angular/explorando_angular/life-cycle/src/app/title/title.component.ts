3134bd1e3f63a7613f04b6458a79961a003f0b5c
willianxzy <willianlittig14@gmail.com>
2024-04-24 21:01:46 -0300

    feat: introdução a OnChanges do LifeCycle Hooks e seu funcionamento na prática

--- fundamentos_angular/explorando_angular/life-cycle/src/app/title/title.component.ts
+++ fundamentos_angular/explorando_angular/life-cycle/src/app/title/title.component.ts
@@ -1,18 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges, Input,} from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.css']
 })
-export class TitleComponent implements OnInit {
+export class TitleComponent implements OnInit, OnChanges {
 
-  nome: string = 'Willian';
+  @Input() nome: string = '';
 
-  constructor() {}
+  constructor() {
+    console.log(`Construtor ${this.nome}`)
+  }
+
+  ngOnChanges(): void {
+    console.log(`OnChange ${this.nome}`)
+  }
 
   ngOnInit(): void {
-    this.nome = `Hello ${this.nome}`;
+    console.log(`OnInit ${this.nome}`)
   }
 
 }
