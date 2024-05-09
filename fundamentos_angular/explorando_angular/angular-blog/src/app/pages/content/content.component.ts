c10faa4307cf7d887dddb6f647e4d6c3497c65a9
willianxzy <willianlittig14@gmail.com>
2024-05-09 13:17:39 -0300

    feat:  import activatedroute + dataFake, atribuindo endereço para sincronizar com contentTitle, contentDescription, photoCover

--- fundamentos_angular/explorando_angular/angular-blog/src/app/pages/content/content.component.ts
+++ fundamentos_angular/explorando_angular/angular-blog/src/app/pages/content/content.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { dataFake } from 'src/app/data/dataFake';
 
 @Component({
   selector: 'app-content',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
+  photoCover:string = "https://i.guim.co.uk/img/media/9b7412a06451584d8e594c9d08b3cd0623d23d02/0_11_2158_1116/master/2158.jpg?width=465&dpr=1&s=none";
+  contentTitle:string = "Noticia Exemplo";
+  contentDescription:string = "bla bla bla bla "
+  private id:string | null = "0"
 
-  constructor() { }
+  constructor(
+    private route:ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe( value =>
+      this.id = (value.get("id"))
+    )
+
+    this.setValuesToComponent(this.id)
   }
 
+  setValuesToComponent(id:string | null) {
+    const result = dataFake.filter(article => article.id == id)[0]
+
+    this.contentTitle = result.title
+    this.contentDescription = result.description
+    this.photoCover = result.photoCover
+  }
 }
