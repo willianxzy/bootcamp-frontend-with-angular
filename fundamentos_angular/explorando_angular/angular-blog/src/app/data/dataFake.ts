13a29328494b60dd167c0ba31d574fee6e9da1b2
willianxzy <willianlittig14@gmail.com>
2024-05-09 13:11:17 -0300

    feat: created dataFake

---
+++ fundamentos_angular/explorando_angular/angular-blog/src/app/data/dataFake.ts
@@ -0,0 +1,15 @@
+
+export const dataFake = [
+  {
+  "id":"1",
+  "title":"NOVO HOMEM DE FERRO EM 3D",
+  "description":"Marvel Studios anuncia novo filme do homem de ferro",
+  "photoCover": "https://i.guim.co.uk/img/media/9b7412a06451584d8e594c9d08b3cd0623d23d02/0_11_2158_1116/master/2158.jpg?width=465&dpr=1&s=none",
+},
+{
+  "id":"2",
+  "title":"CONFIRA OS NOVOS PERSONAGENS DA MARVEL",
+  "description":"Super-Xandão é o mais novo personagem da Saga da Marvel",
+  "photoCover": "https://www.telltalesonline.com/wp-content/uploads/2018/03/popular-marvel-characters.jpg",
+},
+]
