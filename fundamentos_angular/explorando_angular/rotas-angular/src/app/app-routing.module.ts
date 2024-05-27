3b01c8aa7c3e82fa17a14c12cfdb98fae2d4d03a
willianxzy <willianlittig14@gmail.com>
2024-05-27 18:47:59 -0300

    feat: add rota-coringa

--- fundamentos_angular/explorando_angular/rotas-angular/src/app/app-routing.module.ts
+++ fundamentos_angular/explorando_angular/rotas-angular/src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import { CardComponent } from './pages/portifolio/card/card.component';
 
 const routes: Routes = [
   {path: '', component:TitleComponent, pathMatch:'full'},
-  {path: 'portfolio', component: CardComponent, pathMatch:'prefix'}
+  {path: 'portfolio', component: CardComponent, pathMatch:'prefix'},
+  {path: '**', redirectTo:''}
 ];
 
 @NgModule({
