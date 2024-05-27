2fbf572e77872257191e3cfe9ece95a9593f1821
willianxzy <willianlittig14@gmail.com>
2024-05-27 16:37:53 -0300

    refactor

--- fundamentos_angular/explorando_angular/rotas-angular/karma.conf.js
+++ fundamentos_angular/explorando_angular/rotas-angular/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/c'),
+      dir: require('path').join(__dirname, './coverage/rotas-angular'),
       subdir: '.',
       reporters: [
         { type: 'html' },
