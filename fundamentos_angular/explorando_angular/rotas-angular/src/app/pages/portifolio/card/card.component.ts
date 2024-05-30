365cb9137274dbae71c9fc88408201ce4d0bda49
willianxzy <willianlittig14@gmail.com>
2024-05-30 09:56:54 -0300

    feat: Utilizando o ActivatedRouter para receber os parâmetros quando o path: portfolio/:id for acessado

--- fundamentos_angular/explorando_angular/rotas-angular/src/app/pages/portifolio/card/card.component.ts
+++ fundamentos_angular/explorando_angular/rotas-angular/src/app/pages/portifolio/card/card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-card',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private activeRoute: ActivatedRoute) {
+    this.activeRoute.params.subscribe(
+      res => console.log(res)
+    )
+  }
 
   ngOnInit(): void {
   }
