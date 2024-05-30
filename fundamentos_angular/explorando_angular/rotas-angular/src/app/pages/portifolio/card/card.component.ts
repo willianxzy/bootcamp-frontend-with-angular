61ccc8bdc039cd9b9f60c1ec8277661cb43f3567
willianxzy <willianlittig14@gmail.com>
2024-05-30 11:06:48 -0300

    feat: recuperando parâmetros das rotas e exibindo no console.

--- fundamentos_angular/explorando_angular/rotas-angular/src/app/pages/portifolio/card/card.component.ts
+++ fundamentos_angular/explorando_angular/rotas-angular/src/app/pages/portifolio/card/card.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-card',
@@ -8,13 +8,26 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class CardComponent implements OnInit {
 
-  constructor(private activeRoute: ActivatedRoute) {
+  constructor(
+    private activeRoute: ActivatedRoute,
+    //private navegador: Router
+  ) {
+
+    // localhost:4200/portfolio/{1}
     this.activeRoute.params.subscribe(
       res => console.log(res)
     )
+
+    // localhost:4200/potfolio/1?name=willian&token=123
+    //this.activeRoute.queryParams.subscribe(
+      //res => console.log(res)
+    //)
   }
 
   ngOnInit(): void {
+    //setInterval(()=>{
+      //this.navegador.navigate(['/'])
+    //}, 10000)
   }
 
 }
