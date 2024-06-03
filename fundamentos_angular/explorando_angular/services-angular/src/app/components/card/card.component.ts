93f8b4012fb84232376fc75b51464f38237ff0b8
willianxzy <willianlittig14@gmail.com>
2024-06-03 12:27:43 -0300

    feat: transformando o card__title e card__stats__type dinâmicose e fazendo a interpolação deles + utilizando *ngfor p/ trazer nossos attributeTypes em array dinamico.

--- fundamentos_angular/explorando_angular/services-angular/src/app/components/card/card.component.ts
+++ fundamentos_angular/explorando_angular/services-angular/src/app/components/card/card.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
+  name: String = " CHARIZARD "
+  attributesTypes: String[] = ['FIRE', 'ROCK']
 
   constructor() { }
 
